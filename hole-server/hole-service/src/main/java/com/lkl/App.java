7382f228cca15af0cd4002c35546501184bc7bdf
Juntao Han <499445428@qq.com>
2018-05-09 11:04:58 +0800

    Add client and server code

---
+++ hole-server/hole-service/src/main/java/com/lkl/App.java
@@ -0,0 +1,13 @@
+package com.lkl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
